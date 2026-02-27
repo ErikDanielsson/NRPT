71b258ce02d1c36ef7a3a9b4eb69af1c3694d865
ErikDanielsson <danielsson.erik.0@gmail.com>
2026-02-27 11:20:07 +0100

    Restructure repo and update readme

--- src/log_potential/paths.jl
+++ src/log_potential/paths.jl
@@ -1,20 +1,3 @@
-struct SamplingProblem{T, S, W, L, D}
-    V0::T
-    sample_iid::S
-    V::W
-    data::Vector{D}
-    V1::L
-end
-
-function SamplingProblem(V0, sample_iid, V, data)
-    function V1(x)
-        sum(V(x, d) for d in data)
-    end
-    return SamplingProblem(
-        V0, sample_iid, V, data, V1
-    )
-end
-
 abstract type Path end
 
 struct StaticPath <: Path
