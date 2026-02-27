539dac70e4950595c7755b41e563f7f56649ee76
ErikDanielsson <danielsson.erik.0@gmail.com>
2026-02-27 11:20:07 +0100

    Restructure repo and update readme

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # NRPT
 
 [![Build Status](https://github.com/ErikDanielsson/NRPT.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/ErikDanielsson/NRPT.jl/actions/workflows/CI.yml?query=branch%3Amain)
+
+Implementation of non-reverisble parallel tempering with path optimization
+For a production grade implementation please use [Pigeons.jl](https://pigeons.run/stable/).
--- src/NRPT.jl
+++ src/NRPT.jl
@@ -4,34 +4,45 @@ using Distributions, Random, LinearAlgebra, Interpolations, ProgressMeter, ADTyp
 import DifferentiationInterface
 
 include("helpers.jl")
-include("problems.jl")
+include("log_potential/sampling_problems.jl")
+include("log_potential/paths.jl")
+
 include("evidence/stepping_stone.jl")
 include("explorers/explorers.jl")
 include("explorers/mh_kernels.jl")
 include("explorers/slice_sampler.jl")
 
-include("path-problem.jl")
-include("barriers.jl")
+include("log_potential/path_problem.jl")
 
-include("opt/stoch-opt.jl")
-include("opt/proximal-operators.jl")
+include("opt/stoch_opt.jl")
+include("opt/proximal_operators.jl")
 include("opt/optimizers.jl")
+include("opt/path_optimization.jl")
 
-include("stats.jl")
-include("path_adaptation.jl")
+include("stats/barriers.jl")
+include("stats/stats.jl")
+
 include("DEO.jl")
-
 include("nrpt.jl")
 
-export make_q_mala, make_q_grw
+# Explorers
 export MHExplorer, SliceSampler, IterExplorer
-export nrpt, optimized_nrpt
+export make_q_mala, make_q_grw
+
+# Problems and choices of paths
 export SamplingProblem, PathProblem, linear_path, power_path, q_path, spline_path
 export linear_spline, theta_to_eta
+
+# Statistics
 export round_trip_rate, count_chain_round_trips, count_round_trips_per_round
+
+# Optimizaton
 export ProximalStochOptState, NoOptState
 export ProximalState, ProjectionState, Box, project
 export SGDState, DoGState, DoWGState
+
+# Main algorithm
+export nrpt, optimized_nrpt
 
 
 end
